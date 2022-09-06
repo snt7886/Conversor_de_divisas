7db443b34ac093bec6ea9d2e333a71659bdc3d6e
santiago abrate <santiabrate1@gmail.com>
2022-09-05 22:24:00 -0300

    Objeto y Array

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,68 +1,50 @@
-let CambioTotal = 0
-let MonedaACambiar = parseInt(prompt("¿Qué moneda deseas cambiar? 1_Pesos Argentino, 2_Dólar, 3_Euro o 4_Real Brasileño"))
-let MonedaEleccion = true
-let Cantidad
-let MonedaCambio
-let Monto
-while (MonedaEleccion===true){
-    if (MonedaACambiar===1){
-        MonedaCambio = parseInt(prompt("Elija un cambio: 1_Dólar, 2_Euro o 3_Real Brasileño"))
-    } else if (MonedaACambiar===2){
-        MonedaCambio = parseInt(prompt("Elija un cambio: 1_Peso Argentino, 2_Euro o 3_Real Brasileño"))
-    } else if (MonedaACambiar===3){
-        MonedaCambio = parseInt(prompt("Elija un cambio: 1_Peso Argentino, 2_Dólar o 3_Real Brasileño"))
-    } else if (MonedaACambiar===4){
-        MonedaCambio = parseInt(prompt("Elija un cambio: 1_Peso Argentino, 2_Dólar o 3_Euro"))
-    }  else {
-        MonedaACambiar = parseInt(prompt("Porfavor, elija una moneda existente: 1_Peso Argentino, 2_Dólar, 3_Euro o 4_Real Brasileño"))
-        continue
-    }
+let cambioTotal = 0
+let monedaACambiar = parseInt(prompt("¿Con que deseas cambiar tus Dólares? 1_Pesos argentinos, 2_Euros o 3_Reales brasileños"))
+let monedaEleccion = true
+let cantidad
+let monto
 
-    Cantidad = parseInt(prompt("¿Cuanto quiere cambiar?"))
-    function cambio (CambioTotal, Cantidad){
-        if (MonedaACambiar===1 && MonedaCambio===1){
-            CambioTotal = Cantidad*0.00692
-            //Peso a Dólar Oficial
-        } else if (MonedaACambiar===1 && MonedaCambio===2){
-            CambioTotal = Cantidad*0.00693
-            //Peso a Euro Oficial
-        } else if (MonedaACambiar===1 && MonedaCambio===3){
-            CambioTotal = Cantidad*0.036
-            //Peso a Real Brasileño
-        } else if (MonedaACambiar===2 && MonedaCambio===1){
-            CambioTotal = Cantidad*144.50867
+//Objeto
+class Monedas {
+    constructor (nombre,precio){
+        this.nombre = nombre
+        this.precio = precio
+    }
+    cambiarPrecio (nuevoPrecio) {
+        this.precio = nuevoPrecio
+    }
+}
+const pesoArg = new Monedas ("Peso argentino", 144.50867)
+const euro = new Monedas ("Euro", 1.00145)
+const real = new Monedas ("Real brasileño", 5.20231)
+
+
+while (monedaEleccion===true){
+    cantidad = parseInt(prompt("¿Cuanto quiere cambiar?"))
+    function cambio (cambioTotal, cantidad){
+        if (monedaACambiar===1){
+            cambioTotal = cantidad*pesoArg.precio
             //Dólar a Peso Argentino BCRA
-        } else if (MonedaACambiar===2 && MonedaCambio===2){
-            CambioTotal = Cantidad*1.00145
+        } else if (monedaACambiar===2){
+            cambioTotal = cantidad*euro.precio
             //Dólar a Euro
-        } else if (MonedaACambiar===2 && MonedaCambio===3){
-            CambioTotal = Cantidad*5.20231
+        } else if (monedaACambiar===3){
+            cambioTotal = cantidad*real.precio
             //Dólar a Real Brasileño
-        } else if (MonedaACambiar===3 && MonedaCambio===1){
-            CambioTotal = Cantidad*144.30014
-            //Euro a Peso Argentino BCRA
-        } else if (MonedaACambiar===3 && MonedaCambio===2){
-            CambioTotal = Cantidad*0.99855
-            //Euro a Dólar
-        } else if (MonedaACambiar===3 && MonedaCambio===3){
-            CambioTotal = Cantidad*5.19481
-            //Euro a Real Brasileño
-        } else if (MonedaACambiar===4 && MonedaCambio===1){
-            CambioTotal = Cantidad*27.77778
-            //Real a Peso Argentino
-        } else if (MonedaACambiar===4 && MonedaCambio===2){
-            CambioTotal = Cantidad*0.19222
-            //Real a Dólar
-        } else if (MonedaACambiar===4 && MonedaCambio===3){
-            CambioTotal = Cantidad*0.1925
-            //Real a Euro
-        } else {
-           alert ("Porfavor, ingrese una moneda existente") 
+        }else {
+           alert ("Porfavor, ingrese una moneda existente")          
         }
-
-        return CambioTotal
+        return cambioTotal
     }
-    Monto = cambio (CambioTotal, Cantidad)
-    alert ("Tu saldo es de "+Monto)
+    monto = cambio (cambioTotal, cantidad)
+    alert ("Tu saldo es de "+monto)
     break
+}
+let proximamente = prompt("¿Qué divisa le gustaria que agreguemos?")
+let divisas = []
+let i = 1
+while (i<=2) {
+    divisas.push (proximamente)
+    i++
+    console.log (divisas)
 }
