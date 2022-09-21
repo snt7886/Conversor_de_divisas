08dba658007cd274fb93314af0a9ccadac70d86f
santiago abrate <santiabrate1@gmail.com>
2022-09-21 19:20:10 -0300

    JSon

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,10 +1,7 @@
-let cambioTotal = 0
-let monedaACambiar = parseInt(prompt("¿Con que deseas cambiar tus Dólares? 1_Pesos argentinos, 2_Euros o 3_Reales brasileños"))
-let monedaEleccion = true
-let cantidad
-let monto
+let resultado = 0
 
-//Objeto
+
+
 class Monedas {
     constructor (nombre,precio){
         this.nombre = nombre
@@ -14,37 +11,49 @@ class Monedas {
         this.precio = nuevoPrecio
     }
 }
+
 const pesoArg = new Monedas ("Peso argentino", 144.50867)
 const euro = new Monedas ("Euro", 1.00145)
-const real = new Monedas ("Real brasileño", 5.20231)
+const real = new Monedas ("Real", 5.20231)
+
+const divisas = [pesoArg, euro, real]
 
 
-while (monedaEleccion===true){
-    cantidad = parseInt(prompt("¿Cuanto quiere cambiar?"))
-    function cambio (cambioTotal, cantidad){
-        if (monedaACambiar===1){
-            cambioTotal = cantidad*pesoArg.precio
-            //Dólar a Peso Argentino BCRA
-        } else if (monedaACambiar===2){
-            cambioTotal = cantidad*euro.precio
-            //Dólar a Euro
-        } else if (monedaACambiar===3){
-            cambioTotal = cantidad*real.precio
-            //Dólar a Real Brasileño
-        }else {
-           alert ("Porfavor, ingrese una moneda existente")          
-        }
-        return cambioTotal
+
+function cambio (cantidad){
+    if (monedaACambiar===1){
+        resultado = cantidad*divisas[0].precio
+    } else if (monedaACambiar===2){
+        resultado = cantidad*divisas[1].precio
+    } else if (monedaACambiar===3){
+        resultado = cantidad*divisas[2].precio
     }
-    monto = cambio (cambioTotal, cantidad)
-    alert ("Tu saldo es de "+monto)
-    break
 }
-let proximamente = prompt("¿Qué divisa le gustaria que agreguemos?")
-let divisas = []
-let i = 1
-while (i<=2) {
-    divisas.push (proximamente)
-    i++
-    console.log (divisas)
+
+let monedaACambiar = parseInt(prompt("¿Con que deseas cambiar tus Dólares? 1_Pesos argentinos, 2_Euros o 3_Reales brasileños"))
+
+while (monedaACambiar!==1&&monedaACambiar!==2&&monedaACambiar!==3){
+    monedaACambiar = parseInt(prompt("Porfavor, ingrese una moneda existente: 1_Pesos argentinos, 2_Euros o 3_Reales brasileños"))
 }
+
+let cantidadUsuario = parseInt(prompt("¿Cuanto quiere cambiar?"))
+
+cambio(cantidadUsuario)
+
+alert ("Tu saldo es de "+resultado)
+
+
+let usuarioMoneda = prompt("¿Que moneda deseas? Euro, Real o Peso argentino")
+function buscarMoneda(parametro){
+    const found = divisas.find(element => element.nombre === parametro)
+    return found
+}
+const monedaEncontrada = buscarMoneda(usuarioMoneda)
+
+alert ("Tu moneda es "+monedaEncontrada.nombre)
+
+localStorage.setItem("clave", "Hola")
+
+let item = localStorage.getItem("clave")
+
+console.log(item)
